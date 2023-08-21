3fb83b640f2efa19ffc06388c770ff9d7739b9c8
Miguel B. Henriques <103072223+MiguelHenri@users.noreply.github.com>
2023-08-21 17:39:31 -0300

    Create calibration.sol

---
+++ smart-contracts/calibration.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+// Version of compiler
+pragma solidity >=0.7.0 <0.9.0;
+
+contract calibration {
+
+    // array that stores valid robots adresses
+    address[] private robots;
+
+    // mapping to check valid robot addresses
+    mapping (address => bool) private isRobot;
+
+    // mapping to store calibration data
+    mapping (address => string) private data;
+
+    constructor(address[] memory _robots) {
+        robots = _robots;
+        for (uint256 i = 0; i < robots.length; i++) {
+            isRobot[robots[i]] = true;
+        }
+    }
+
+    function sendCalibrationData(address _robot, string memory _info) public {
+        
+        require(isRobot[_robot] == true, "it is not a valid robot");
+
+        // require technician
+
+        data[_robot] = _info;
+
+    }
+
+}
