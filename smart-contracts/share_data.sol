ab58b29637abde5f28dfbb8c414e2f67d622be70
Miguel B. Henriques <103072223+MiguelHenri@users.noreply.github.com>
2023-09-08 13:02:26 -0300

    Update share_data.sol

--- smart-contracts/share_data.sol
+++ smart-contracts/share_data.sol
@@ -4,18 +4,6 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract shareData {
 
-    // string that stores the place of the inspection task
-    string private place;
-
-    // string that stores the date of the inspection task
-    string private date;
-
-    // array that stores robots addresses
-    address[] private robots;
-
-    // mapping to check valid robot addresses
-    mapping (address => bool) private isRobot;
-
     struct Time {
         uint32 secs;
         uint32 nsecs;
@@ -32,6 +20,38 @@ contract shareData {
         int256 variance;
     }
 
+    struct Vector3 {
+        uint32 x;
+        uint32 y;
+        uint32 z;
+    }
+
+    struct Quaternion {
+        uint32 x;
+        uint32 y;
+        uint32 z;
+        uint32 w;
+    }
+
+    struct Pose {
+        Vector3 position;
+        Quaternion orientation;
+        uint32[36] covariance;
+    }
+
+    struct Twist {
+        Vector3 linear;
+        Vector3 angular;
+        uint32[36] covariance;
+    }
+
+    struct Odom {
+        Header header;
+        string child_frame_id;
+        Pose pose;
+        Twist twist;
+    }
+
     struct Temperature {
         Header header;
         Data data;
@@ -46,12 +66,27 @@ contract shareData {
         string IPFS_Cid;
     }
 
-    // public variables that store robots data
-    mapping (address => Temperature) temperature;
-    mapping (address => Humidity) humidity;
-    mapping (address => PointCloud) pointCloud;
+    // string that stores the place of the inspection task
+    string private place;
 
-    // function called in contract deployment
+    // string that stores the date of the inspection task
+    string private date;
+
+    // variable that stores the inspector address
+    address private inspector;
+
+    // array that stores robots adresses
+    address[] private robots;
+
+    // mapping to check valid robot addresses
+    mapping (address => bool) private isRobot;
+
+    // public variables that store robots data
+    mapping (address => Temperature) temperatureData;
+    mapping (address => Humidity) humidityData;
+    mapping (address => PointCloud) pointCloudData;
+    mapping (address => Odom) odomData;
+
     constructor(string memory _place, string memory _date, address[] memory _robots) {
         place = _place;
         date = _date;
@@ -65,12 +100,12 @@ contract shareData {
 
         require(isRobot[msg.sender] == true, "sender is not a robot");
 
-        temperature[msg.sender].header.seq = _temp.header.seq;
-        temperature[msg.sender].header.frame_id = _temp.header.frame_id;
-        temperature[msg.sender].header.stamp.secs = _temp.header.stamp.secs;
-        temperature[msg.sender].header.stamp.nsecs = _temp.header.stamp.nsecs;
-        temperature[msg.sender].data.value = _temp.data.value;
-        temperature[msg.sender].data.variance = _temp.data.variance;
+        temperatureData[msg.sender].header.seq = _temp.header.seq;
+        temperatureData[msg.sender].header.frame_id = _temp.header.frame_id;
+        temperatureData[msg.sender].header.stamp.secs = _temp.header.stamp.secs;
+        temperatureData[msg.sender].header.stamp.nsecs = _temp.header.stamp.nsecs;
+        temperatureData[msg.sender].data.value = _temp.data.value;
+        temperatureData[msg.sender].data.variance = _temp.data.variance;
 
     }
 
@@ -78,12 +113,12 @@ contract shareData {
 
         require(isRobot[msg.sender] == true, "sender is not a robot");
 
-        humidity[msg.sender].header.seq = _hum.header.seq;
-        humidity[msg.sender].header.frame_id = _hum.header.frame_id;
-        humidity[msg.sender].header.stamp.secs = _hum.header.stamp.secs;
-        humidity[msg.sender].header.stamp.nsecs = _hum.header.stamp.nsecs;
-        humidity[msg.sender].data.value = _hum.data.value;
-        humidity[msg.sender].data.variance = _hum.data.variance;
+        humidityData[msg.sender].header.seq = _hum.header.seq;
+        humidityData[msg.sender].header.frame_id = _hum.header.frame_id;
+        humidityData[msg.sender].header.stamp.secs = _hum.header.stamp.secs;
+        humidityData[msg.sender].header.stamp.nsecs = _hum.header.stamp.nsecs;
+        humidityData[msg.sender].data.value = _hum.data.value;
+        humidityData[msg.sender].data.variance = _hum.data.variance;
 
     }
 
@@ -91,8 +126,55 @@ contract shareData {
         
         require(isRobot[msg.sender] == true, "sender is not a robot");
 
-        pointCloud[msg.sender]f.IPFS_Cid = _IPFS_Cid;
+        pointCloudData[msg.sender].IPFS_Cid = _IPFS_Cid;
 
+    }
+
+    // need to change parameters !
+    function updateOdom(
+        uint32 seq,
+        uint32 stampSecs,
+        uint32 stampNsecs,
+        string memory frameId,
+        string memory childFrameId,
+        uint32[3] memory position,
+        uint32[4] memory orientation,
+        uint32[36] memory poseCovariance,
+        uint32[3] memory linear,
+        uint32[3] memory angular,
+        uint32[36] memory twistCovariance
+    ) public {
+        odomData[msg.sender].header.seq = seq;
+        odomData[msg.sender].header.stamp.secs = stampSecs;
+        odomData[msg.sender].header.stamp.nsecs = stampNsecs;
+        odomData[msg.sender].header.frame_id = frameId;
+
+        odomData[msg.sender].child_frame_id = childFrameId;
+
+        odomData[msg.sender].pose.position.x = position[0];
+        odomData[msg.sender].pose.position.y = position[1];
+        odomData[msg.sender].pose.position.z = position[2];
+
+        odomData[msg.sender].pose.orientation.x = orientation[0];
+        odomData[msg.sender].pose.orientation.y = orientation[1];
+        odomData[msg.sender].pose.orientation.z = orientation[2];
+        odomData[msg.sender].pose.orientation.w = orientation[3];
+
+        for (uint256 i = 0; i < 36; i++) {
+            odomData[msg.sender].pose.covariance[i] = poseCovariance[i];
+        }
+
+        odomData[msg.sender].twist.linear.x = linear[0];
+        odomData[msg.sender].twist.linear.y = linear[1];
+        odomData[msg.sender].twist.linear.z = linear[2];
+
+        odomData[msg.sender].twist.angular.x = angular[0];
+        odomData[msg.sender].twist.angular.y = angular[1];
+        odomData[msg.sender].twist.angular.z = angular[2];
+
+        for (uint256 i = 0; i < 36; i++) {
+            odomData[msg.sender].twist.covariance[i] = twistCovariance[i];
+        }
     }
 
 }
